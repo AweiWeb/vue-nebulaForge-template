faf9dfad3386e901187799bd600126052e45a259
13619226460 <2319035503@qq.com>
2023-06-18 10:52:19 +0800

    Layout 路由 挂载子路由的实现⚓️

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,51 @@
-import { createWebHashHistory, createRouter } from 'vue-router';
+import {createRouter, createWebHistory} from 'vue-router';
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes: [
-    {
-      path: '/',
-      component: () => import('@/view/Home/index.vue'),
-    },
-  ],
+    history: createWebHistory(),
+    routes: [
+      {
+            path: '/login',
+            component: () => import('../view/Login/index.vue'),
+            meta: {title: '登录页面'}
+        },
+      {   
+          // 组件的父级路由，其他组件全挂载在上面
+            path: '/',
+            component: () => import('../view/Layout/index.vue'),
+            redirect: '/home',
+            children: [
+                {
+                    path: '/home',
+                    component: () => import('../view/Home/index.vue'),
+                    meta: {title: '主页'}
+                },
+                {
+                    path: '/intro',
+                    component: () => import('../view/Introduce/index.vue')
+                },
+                {
+                    path: '/person',
+                    component: () => import('../view/Personal/index.vue')
+                },
+                {
+                    path: '/play',
+                    component: () => import('../view/Play/index.vue')
+                },
+                {
+                    path: '/approve',
+                    component: () => import('../view/approve/index.vue')
+                }
+            ]
+        }
+    ]
 });
 
+// const whiteList = ['/login', '/404'];
+
+// 导航前置守卫
+router.beforeEach(() => {
+    // 这里进行守卫
+    // 如果，没有登录，就跳回登录页面
+    // 判断有无token进行拦截
+});
 export default router;
